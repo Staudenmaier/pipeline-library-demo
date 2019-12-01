1ac0d8c7e3ce945efb423e07d345f43dbab82d82
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-01 22:14:48 +0100

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,5 +2,9 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+println new URL( "https://query.yahooapis.com/v1/public/yql?q=" +
+        URLEncoder.encode(
+                "select wind from weather.forecast where woeid in " +
+                        "(select woeid from geo.places(1) where text='chicago, il')",
+                'UTF-8' ) ).text
 }
-
