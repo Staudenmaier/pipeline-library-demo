fb67d2d110dedbf107cd7dc113914f99450446bd
Staudenmaier <baustelleninfo@rocketmail.com>
2019-12-01 23:24:01 +0100

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,7 +2,7 @@
 def call(String name = 'human') {
   echo "Hello, ${name}."
   echo "-----------------------------"
-  echo ${env.JOB_NAME}
+  echo "${env.JOB_NAME}"
    echo "-----------------------------"
 
 
